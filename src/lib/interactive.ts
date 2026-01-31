e506d08540a21a95f41c6e3ac5078971f3bca1d5
Jóhann S. Ingason <jsingason@gmail.com>
2026-01-31 21:33:27 +0000

    Updated dependencies and selection lib

--- src/lib/interactive.ts
+++ src/lib/interactive.ts
@@ -1,17 +1,22 @@
-import inquirer from 'inquirer';
+import { select, Separator } from '@inquirer/prompts';
 import { getPackageJsonScripts } from './pm';
 import { getGlobalScripts, getDirectoryScripts } from './scripts';
 import { runPackageScript, runDirectoryScript, runGlobalScript } from './run';
+
+interface ScriptChoice {
+  name: string;
+  value: { type: string; script: string };
+}
 
 export const interactiveMode = async () => {
   const packageScripts = getPackageJsonScripts();
   const globalScripts = getGlobalScripts();
   const directoryScripts = getDirectoryScripts();
 
-  const choices = [];
+  const choices: (Separator | ScriptChoice)[] = [];
 
   if (Object.keys(packageScripts).length > 0) {
-    choices.push(new inquirer.Separator('--- Package Scripts ---'));
+    choices.push(new Separator('--- Package Scripts ---'));
     choices.push(
       ...Object.keys(packageScripts).map((script) => ({
         name: `${script}`,
@@ -21,7 +26,7 @@ export const interactiveMode = async () => {
   }
 
   if (Object.keys(directoryScripts).length > 0) {
-    choices.push(new inquirer.Separator('--- Directory Scripts ---'));
+    choices.push(new Separator('--- Directory Scripts ---'));
     choices.push(
       ...Object.keys(directoryScripts).map((script) => ({
         name: `${script}`,
@@ -31,7 +36,7 @@ export const interactiveMode = async () => {
   }
 
   if (Object.keys(globalScripts).length > 0) {
-    choices.push(new inquirer.Separator('--- Global Scripts ---'));
+    choices.push(new Separator('--- Global Scripts ---'));
     choices.push(
       ...Object.keys(globalScripts).map((script) => ({
         name: `${script}`,
@@ -39,15 +44,12 @@ export const interactiveMode = async () => {
       })),
     );
   }
-  const { selection } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'selection',
-      message: 'Select a script to run:',
-      choices,
-      loop: false,
-    },
-  ]);
+
+  const selection = await select({
+    message: 'Select a script to run:',
+    choices,
+    loop: false,
+  });
 
   if (selection.type === 'package') {
     runPackageScript(selection.script);
